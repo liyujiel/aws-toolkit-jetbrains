46ac1a91f466a0c05b3eeda423601c168e4322ab
Hunter Werlla <werlla@amazon.com>
2020-12-04 09:20:12 -0800

    Start making some progress

---
+++ jetbrains-ultimate/src/software/aws/toolkits/jetbrains/services/lambda/go/GoLambdaBuilder.kt
@@ -0,0 +1,26 @@
+// Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package software.aws.toolkits.jetbrains.services.lambda.go
+
+import com.intellij.openapi.application.ReadAction
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ProjectFileIndex
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiElement
+import software.aws.toolkits.jetbrains.services.lambda.LambdaBuilder
+import software.aws.toolkits.jetbrains.services.lambda.nodejs.inferSourceRoot
+import java.nio.file.Path
+import java.nio.file.Paths
+
+class GoLambdaBuilder : LambdaBuilder() {
+    // TODO write
+    override fun handlerBaseDirectory(module: Module, handlerElement: PsiElement): Path {
+        val handlerVirtualFile = ReadAction.compute<VirtualFile, Throwable> {
+            handlerElement.containingFile?.virtualFile
+                ?: throw IllegalArgumentException("Handler file must be backed by a VirtualFile")
+        }
+        return Paths.get(ProjectFileIndex.getInstance(module.project).path)
+    }
+}
---
+++ jetbrains-ultimate/src/software/aws/toolkits/jetbrains/services/lambda/go/GoSamProjectWizard.kt
@@ -0,0 +1,72 @@
+// Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package software.aws.toolkits.jetbrains.services.lambda.go
+
+import com.intellij.javascript.nodejs.interpreter.NodeJsInterpreterField
+import com.intellij.javascript.nodejs.interpreter.NodeJsInterpreterManager
+import com.intellij.javascript.nodejs.interpreter.NodeJsInterpreterRef
+import com.intellij.lang.javascript.dialects.JSLanguageLevel
+import com.intellij.lang.javascript.settings.JSRootConfiguration
+import com.intellij.openapi.project.ProjectManager
+import com.intellij.openapi.roots.ModifiableRootModel
+import com.intellij.openapi.ui.TextFieldWithBrowseButton
+import com.intellij.openapi.ui.ValidationInfo
+import software.amazon.awssdk.services.lambda.model.PackageType
+import software.amazon.awssdk.services.lambda.model.Runtime
+import software.aws.toolkits.jetbrains.services.lambda.nodejs.NodeJsSdkSelectionPanel
+import software.aws.toolkits.jetbrains.services.lambda.nodejs.SamHelloWorldNodeJs
+import software.aws.toolkits.jetbrains.services.lambda.wizard.SamAppTemplateBased
+import software.aws.toolkits.jetbrains.services.lambda.wizard.SamProjectTemplate
+import software.aws.toolkits.jetbrains.services.lambda.wizard.SamProjectWizard
+import software.aws.toolkits.jetbrains.services.lambda.wizard.SdkSelector
+import software.aws.toolkits.jetbrains.utils.ui.validationInfo
+import software.aws.toolkits.resources.message
+import javax.swing.JComponent
+import javax.swing.JLabel
+
+class GoSamProjectWizard  : SamProjectWizard {
+    override fun createSdkSelectionPanel(projectLocation: TextFieldWithBrowseButton?): SdkSelector = GoSdkSelectionPanel()
+
+    override fun listTemplates(): Collection<SamProjectTemplate> = listOf(
+        SamHelloWorldGo()
+    )
+}
+
+class GoSdkSelectionPanel : SdkSelector {
+    private val interpreterPanel = createInterpreterField()
+
+    private fun createInterpreterField(): NodeJsInterpreterField {
+        val project = ProjectManager.getInstance().defaultProject
+        return object : NodeJsInterpreterField(project, false) {
+            override fun isDefaultProjectInterpreterField(): Boolean = true
+        }
+    }
+
+    override fun sdkSelectionLabel() = JLabel(message("sam.init.node_interpreter.label"))
+
+    override fun sdkSelectionPanel(): JComponent = interpreterPanel
+
+    override fun applySdkSettings(model: ModifiableRootModel) {
+        NodeJsInterpreterManager.getInstance(model.project).setInterpreterRef(NodeJsInterpreterRef.create(interpreterPanel.interpreter))
+        JSRootConfiguration.getInstance(model.project).storeLanguageLevelAndUpdateCaches(JSLanguageLevel.ES6)
+    }
+
+    override fun validateSelection(): ValidationInfo? = interpreterPanel.interpreter?.validate(null)?.let {
+        interpreterPanel.validationInfo(it)
+    }
+}
+
+class SamHelloWorldGo : SamAppTemplateBased() {
+    override fun displayName() = message("sam.init.template.hello_world.name")
+
+    override fun description() = message("sam.init.template.hello_world.description")
+
+    override fun supportedRuntimes(): Set<Runtime> = setOf(Runtime.GO1_X)
+
+    override fun supportedPackagingTypes(): Set<PackageType> = setOf(PackageType.IMAGE, PackageType.ZIP)
+
+    override val appTemplateName: String = "hello-world"
+
+    override val dependencyManager: String = "mod"
+}
